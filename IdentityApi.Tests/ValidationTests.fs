fa52c944515c698231333d9b6682de1c8259078c
Michal Moudrý <moudry.michal@live.com>
2022-09-04 13:19:00 +0200

    Changes to validation unit tests

--- IdentityApi.Tests/ValidationTests.fs
+++ IdentityApi.Tests/ValidationTests.fs
@@ -1,17 +1,29 @@
 module ValidationTests
 
+open Xunit
 open IdentityApi.Models
 open IdentityApi.Validators
-open Xunit
 
 [<Fact>]
-let ``Register command validation`` () =
+let ``Register model validation`` () =
     let validator = new RegisterModelValidator()
-    let testData = [|
-        new RegisterModel(UserName = "Test username 1", Email = "test1@test.com", Password = "95UQTVQ!LTWtP")
-        new RegisterModel(UserName = "T2", Email = "test2@test.com", Password = "95UQTVQ!LTWtP")
-        new RegisterModel(UserName = "", Email = "test3@test.com", Password = "95UQTVQ!LTWtP")
-        new RegisterModel(UserName = "Test username 4", Email = "test4@test.com", Password = "95UQTVQ!LTWtP")
-    |]
-    let res = testData |> Array.filter (fun i -> validator.Validate(i).IsValid)
-    Assert.Equal(2, res.Length)
+    let testData = dict [
+        new RegisterModel(UserName = "Test username 1", Email = "test1@test.com", Password = "95UQTVQ!LTWtP"), true
+        new RegisterModel(UserName = "T2", Email = "test2@test.com", Password = "95UQTVQ!LTWtP"), false
+        new RegisterModel(UserName = "", Email = "test3@test.com", Password = "95UQTVQ!LTWtP"), false
+        new RegisterModel(UserName = "Test username 4", Email = "test4@test.com", Password = "95UQTVQ!LTWtP"), true
+    ]
+    for dataItem in testData do
+        Assert.Equal(dataItem.Value, validator.Validate(dataItem.Key).IsValid)
+
+[<Fact>]
+let ``Login model validation`` () =
+    let validator = new LoginModelValidator()
+    let testData = dict [
+        new LoginModel(Email = "test1@test.com", Password = "Q3KTRQG!GLL"), true
+        new LoginModel(Email = "@test.com", Password = "Q3KTRQG!GLL"), false
+        new LoginModel(Email = "", Password = "Q3KTRQG!GLL"), false
+        new LoginModel(Email = "test4@test.com", Password = ""), false
+    ]
+    for dataItem in testData do
+        Assert.Equal(dataItem.Value, validator.Validate(dataItem.Key).IsValid)
--- IdentityApi/Validators/RegisterModelValidator.cs
+++ IdentityApi/Validators/RegisterModelValidator.cs
@@ -8,7 +8,7 @@ public class RegisterModelValidator : AbstractValidator<RegisterModel>
     public RegisterModelValidator()
     {
         RuleFor(x => x.UserName).NotNull().NotEmpty().MinimumLength(3);
-        RuleFor(x => x.Email).NotNull().NotEmpty().MinimumLength(5);
+        RuleFor(x => x.Email).NotNull().NotEmpty().MinimumLength(5).EmailAddress();
         RuleFor(x => x.Password).NotNull().NotEmpty().MinimumLength(7);
     }
 }
