4c3511d04e0a83795d16f5f5a1e4881c5330bdfc
Michal Moudrý <moudry.michal@live.com>
2022-10-27 22:05:45 +0200

    Switched to F# for unit tests

---
+++ IdentityApi.UnitTests/RouterHelperTests.fs
@@ -0,0 +1,30 @@
+module RouterHelperTests
+
+open AssertFunctions
+open System
+open System.Security.Claims
+open System.IdentityModel.Tokens.Jwt
+open Microsoft.IdentityModel.Tokens
+open IdentityApi.Helpers
+open Xunit
+
+let routeHelper = new RouteHelper()
+
+/// Test for CreateErrorMessage() method.
+[<Fact>]
+let TestCreateErrorMessage () =
+    let errors = ["Test"; ""; "Test message"]
+    routeHelper.CreateErrorMessage(errors) |> Equal("Test\n\nTest message")
+
+/// Method for testing successful validation of a correct JWT token.
+[<Fact>]
+let TestCreateJwtToken () =
+    let key = routeHelper.CreateSigningKey("--I3Q5TTGQW!ETG:W!L4(!2::4Q..11111skgpw)))-)ù-§§")
+    let token = routeHelper.CreateJwtToken("test_issuer", "test_audience", [new Claim("id", "idvalue")], key)
+    let tokenHandler = new JwtSecurityTokenHandler()
+    let validationParameters = new TokenValidationParameters(
+        ValidateAudience = true, ValidateIssuer = true, ValidIssuer = "test_issuer", ValidAudience = "test_audience", IssuerSigningKey = key
+    )
+    token |> NotNullOrEmpty
+    let (_, validatedToken) = tokenHandler.ValidateToken(token, validationParameters)
+    validatedToken.ValidTo |> Equal (DateTime.Now.AddDays(1).Date)
---
+++ IdentityApi.UnitTests/ValidationTests.fs
@@ -0,0 +1,20 @@
+﻿module ValidationTests
+
+open AssertFunctions
+open System.Collections.Generic
+open IdentityApi.Validators
+open IdentityApi.Models
+open Xunit
+
+/// Method for testing user model validation.
+[<Fact>]
+let UserModelValidation () =
+    let validator = new UserModelValidator()
+    let testData = new Dictionary<UserModel, bool>();
+    testData.Add(new UserModel(UserName = "test1", Email = "test1@test.com", Password = "Q3KTRQG!GLL"), true)
+    testData.Add(new UserModel(UserName = "test2", Email = "@test.com", Password = "Q3KTRQG!GLL"), false)
+    testData.Add(new UserModel(UserName = "", Email = "test3@test.com", Password = "Q3KTRQG!GLL"), false)
+    testData.Add(new UserModel(UserName = "test4", Email = "test4@test.com", Password = ""), false)
+    testData.Add(new UserModel(UserName = "t5", Email = "test5@test.com", Password = "1"), false)
+    for item in testData do
+        validator.Validate(item.Key).IsValid |> Equal item.Value
