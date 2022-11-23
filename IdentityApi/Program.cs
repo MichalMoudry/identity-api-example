ea463d4b7f875230ade007baa8f02188d5d4262e
Michal Moudrý <moudry.michal@live.com>
2022-11-23 13:39:49 +0100

    Program.cs refactoring

--- IdentityApi/Program.cs
+++ IdentityApi/Program.cs
@@ -1,16 +1,11 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using IdentityApi.Infrastructure;
-using IdentityApi.Models;
-using IdentityApi.Helpers;
-using IdentityApi.Extensions;
 using IdentityApi.Infrastructure.Repositories;
-using Microsoft.AspNetCore.Authorization;
+using IdentityApi;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
-var endpointHelper = new RouteHelper();
 
 var securityKey = builder.Environment.IsDevelopment()
     ? builder.Configuration["SecurityKey"]
@@ -19,7 +14,6 @@ if (securityKey == null)
 {
     throw new NullReferenceException("Security key is null.");
 }
-var signingKey = endpointHelper.CreateSigningKey(securityKey);
 
 var connectionString = builder.Environment.IsDevelopment()
     ? builder.Configuration["ConnectionString"]
@@ -31,11 +25,11 @@ if (connectionString == null)
 }
 
 // Add services to the container.
-builder.Services
-    .AddScoped<IUserRepository, UserRepository>()
-    .AddEndpointsApiExplorer()
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddEndpointsApiExplorer()
     .AddSwaggerGen()
-    .AddDbContext<ApiDbContext>(options => options.UseSqlServer(connectionString))
+    .Configure<SwaggerGeneratorOptions>(options => options.InferSecuritySchemes = true);
+builder.Services.AddDbContext<ApiDbContext>(options => options.UseSqlServer(connectionString))
     .AddIdentity<IdentityUser, IdentityRole>()
     .AddDefaultTokenProviders()
     .AddEntityFrameworkStores<ApiDbContext>();
@@ -44,22 +38,7 @@ builder.Services
 builder.Services.Configure<IdentityOptions>(options =>
 {
     options.User.RequireUniqueEmail = true;
-})
-.AddAuthentication(options =>
-{
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
-    {
-        ValidAudience = builder.Configuration["Audience"],
-        ValidIssuer = builder.Configuration["Issuer"],
-        IssuerSigningKey = signingKey,
-    };
-});
+}).AddAuthentication().AddJwtBearer();
 builder.Services.AddAuthorization();
 builder.Services.AddHealthChecks();
 
@@ -79,67 +58,6 @@ else
     app.UseHttpsRedirection();
 }
 
-// Register route.
-app.MapPost("/register", async ([FromBody] RegisterModel model, IUserRepository userRepository) =>
-{
-    var validationResult = model.Validate();
-    if (!validationResult.IsValid)
-    {
-        var errors = validationResult.Errors;
-        return Results.BadRequest(endpointHelper.CreateErrorMessage(errors));
-    }
-    var (createResult, user) = await userRepository.CreateUserAsync(model.UserName, model.Email, model.Password);
-    if (!createResult.Succeeded)
-    {
-        return Results.BadRequest();
-    }
-    var addToRoleResult = await userRepository.AddUserToRolesAsync(user, "user");
-    if (!addToRoleResult.Succeeded)
-    {
-        return Results.Problem();
-    }
-    return Results.Ok("User registered.");
-})
-.WithName("Register").AddDefaultStatusCodes();
-
-// Login route.
-app.MapPost("/login", async ([FromBody] LoginModel model, IUserRepository userRepository, SignInManager<IdentityUser> signInManager) => {
-    var signInResult = await signInManager.PasswordSignInAsync(model.UserName, model.Password, true, false);
-    if (!signInResult.Succeeded)
-    {
-        return Results.BadRequest();
-    }
-    var (user, roles) = await userRepository.GetUserByUserName(model.UserName);
-    var claims = endpointHelper.CreateClaimsForDefaultUser(user.Id, user.Email, user.UserName, roles);
-    return Results.Ok(new
-    {
-        User = user.Id,
-        Token = endpointHelper.CreateJwtToken(builder.Configuration["Issuer"], builder.Configuration["Audience"], claims, signingKey)
-    });
-})
-.WithName("Login").AddDefaultStatusCodes();
-
-// Edit account route.
-app.MapPut("/edit/{id}", [Authorize] (IUserRepository userRepository, string id) => {
-
-})
-.WithName("Edit account").RequireAuthorization().AddDefaultStatusCodes();
-
-// Delete account route.
-app.MapDelete("/delete/{id}", [Authorize] (IUserRepository userRepository, string id) => {
-    
-})
-.WithName("Delete account").AddDefaultStatusCodes();
-
-// Reset password account route.
-app.MapPut("/resetpassword/{id}", [Authorize] async (string id, IUserRepository userRepository, [FromBody] PasswordResetModel model) => {
-    var resetResult = await userRepository.ResetPassword(id, model.NewPassword);
-    if (!resetResult.Succeeded)
-    {
-        return Results.BadRequest();
-    }
-    return Results.Ok("Password was reset.");
-})
-.WithName("Reset password").AddDefaultStatusCodes();
+app.MapGroup("/users").MapUsers(securityKey);
 
 app.Run();
